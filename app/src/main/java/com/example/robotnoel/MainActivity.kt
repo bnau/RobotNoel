c8c2e8f7936b942608156b491ecf57fb57cad8eb
bnau <bnau@takima.fr>
2020-12-27 18:07:22 +0100

    feat: Communication with serial port

--- app/src/main/java/com/example/robotnoel/MainActivity.kt
+++ app/src/main/java/com/example/robotnoel/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.robotnoel
 
+import android.hardware.usb.UsbManager
 import android.os.Bundle
 import android.view.View
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.hoho.android.usbserial.driver.UsbSerialPort
+import com.hoho.android.usbserial.driver.UsbSerialProber
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,17 +16,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // initiate  views
 
-        // initiate  views
         val simpleSeekBar = findViewById<View>(R.id.seekBar) as SeekBar
         val textView = findViewById<View>(R.id.hw) as TextView
 
-        // perform seek bar change listener event used for getting the progress value
-        // perform seek bar change listener event used for getting the progress value
+        val manager = getSystemService(USB_SERVICE) as UsbManager
+        val availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager)
+        if (availableDrivers.isEmpty()) {
+            return
+        }
+
+        val driver = availableDrivers[0]
+        val connection = manager.openDevice(driver.device)?:return
+
+        val port = driver.ports[0]
+
+        port.open(connection)
+        port.setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE)
+
         simpleSeekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
                 textView.text = "Hello $progress"
+                port.write("${progress + 1}\n".toByteArray(), 20);
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
